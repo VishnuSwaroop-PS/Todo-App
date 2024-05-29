6cc3a952edc10a08f8659a16990529022749aaf4
VishnuSwaroop-PS <vishnuswaroop20@gmail.com>
2024-05-29 19:08:02 +0530

    edit and delete buttons working

--- main.js
+++ main.js
@@ -100,7 +100,23 @@ function DisplayTodos() {
             DisplayTodos();
         })
 
-    
+        edit.addEventListener('click', e => {
+            const input = content.querySelector('input');
+            input.removeAttribute('readonly');
+            input.focus();
+            input.addEventListener('blur', e => {
+                input.setAttribute('readonly',true);
+                todo.content = e.target.value;
+                localStorage.setItem('todos',JSON.stringify(todos));
+                DisplayTodos();
+            })
+        })
+
+        deleteButton.addEventListener('click', e => {
+            todos = todos.filter(t => t != todo);
+            localStorage.setItem('todos', JSON.stringify(todos));
+            DisplayTodos();
+        })
 
     })
 }
